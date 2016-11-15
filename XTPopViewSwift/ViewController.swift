567cf4306ebac16e8332496b779e7248127e28be
summerxx27 <summerxx@163.com>
2016-11-15 12:43:01 +0800

    update
    update

--- XTPopViewSwift/ViewController.swift
+++ XTPopViewSwift/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var btn = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        btn = UIButton.init(type: UIButtonType.custom)
+        btn.frame = CGRect.init(x: 0, y: 0, width: 88, height: 88)
+        btn.center = self.view.center
+        self.view.addSubview(btn)
+        btn.backgroundColor = UIColor.cyan
+        btn.addTarget(self, action: #selector(ViewController.btnClick), for: UIControlEvents.touchUpInside)
+    }
+    func btnClick(){
+        let popView = XTPopViewSwift.init(origin: CGPoint.init(x: self.btn.center.x, y: self.btn.center.y), width: 100, height: 200)
+        popView.bgView.backgroundColor = UIColor.black
+        popView.popView()
         
     }
 
